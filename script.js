a60c3eb5ac08241353007b220d25c3b8a130ed56
tanzilamd <mdtanzil858@gmail.com>
2021-07-28 11:00:54 +0600

    dark btn on function added

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const settingsMenu = document.querySelector(".settings-menu");
+const darkBtn = document.getElementById("dark-btn");
 
 function settinsMenuToggle () {
     settingsMenu.classList.toggle("settings-menu-height")
 }
+
+darkBtn.onclick = function () {
+    darkBtn.classList.toggle("dark-btn-on");
+}
+
